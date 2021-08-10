986661522d52996da59ff27da15fc1ec3a80237b
yarastqt <yarastqt@gmail.com>
2021-08-10 17:34:14 +0300

    feat: impl base plugin

--- packages/babel-plugin/src/index.ts
+++ packages/babel-plugin/src/index.ts
@@ -1,1 +1,20 @@
-export default function plugin() {}
+import { declare } from '@babel/helper-plugin-utils'
+import { types as t } from '@babel/core'
+
+export default declare((api, options) => {
+  api.assertVersion(7)
+
+  return {
+    name: '@compiled-css/babel-plugin',
+    visitor: {
+      Program: {
+        enter: (path, state) => {
+          path.traverse({
+            ImportDeclaration: () => {},
+            TaggedTemplateExpression: () => {},
+          })
+        },
+      },
+    },
+  }
+})
